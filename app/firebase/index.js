f13f7b0f3dd9642cd38854712e86420fdf7a4be7
Urbino Pescada <urbinopescada@gmail.com>
2017-04-24 16:38:11 +0100

    move firebase credaential to environment variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
-
+ 
 try {
   var config = {
-    apiKey: "AIzaSyCSurAqXnLzo_ljXvU_xS9FveAr_btK7-s",
-    authDomain: "mead-todo-app-cf99e.firebaseapp.com",
-    databaseURL: "https://mead-todo-app-cf99e.firebaseio.com",
-    projectId: "mead-todo-app-cf99e",
-    storageBucket: "mead-todo-app-cf99e.appspot.com",
-    messagingSenderId: "955533530547"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
 
   // Initialize Firebase
